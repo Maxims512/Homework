87f2cd6a821c0fcaa4aff46f57575fd9b5eb608c
maximix <ivan82kaktus@mail.ru>
2023-04-01 00:08:55 +0400

    Task6_2

---
+++ Task6/Task6_2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+bool inList(list<int> list, int ch){
+    for (auto i = list.begin(); i != list.end(); i++){
+        if(*i==ch) return true;
+    }
+    if (ch==*list.end()) return true;
+}
+
+void printList(list<int> list){
+    for (auto i = list.begin(); i != list.end(); i++){
+        cout<<*i<<" ";
+    }
+    cout<<endl;
+}
+
+list<int> uniqe(list<int> list1){
+    auto it = list1.end();
+    list<int> list2;
+    it--;
+    for (it; it != list1.begin(); it--){
+        if (!(inList(list2, *it))){
+            list2.push_front(*it);
+        }
+    }
+    if (!(inList(list2, *list1.begin()))){
+        list2.push_front(*list1.begin());
+    }
+    return list2;
+}
+
+int main(){
+    list<int> list2;
+    list<int> list = {5,55,4,7,7,6,8,6,5,4,3,5,7};
+
+    printList(list);
+
+    list2 = uniqe(list);
+
+    printList(list2);
+}
